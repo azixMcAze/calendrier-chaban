79eb85f16c71b41f1769cb967cd9dac9c97e4d6c
mathilde <mathilde.ferrier@gmail.com>
2024-06-09 22:08:32 +0200

    write calendar

--- parse_bridge_json.py
+++ parse_bridge_json.py
@@ -1,15 +1,17 @@
-import json
-import sys
 import datetime
+import icalendar
+import json
 import pytz
+import sys
 
 TZ_NAME = 'Europe/Paris'
 TZ_DST = True
 
-def main(json_filename: str):
+def main(json_filename: str, ical_filename: str):
     with open(json_filename, 'r') as fs:
         json_data = json.load(fs)
 
+    cal = icalendar.Calendar()
     tz = pytz.timezone(TZ_NAME)
 
     for json_item in json_data['results']:
@@ -19,8 +21,15 @@ def main(json_filename: str):
         reopening_time = datetime.time.fromisoformat(json_item['re_ouverture_a_la_circulation'])
         reopening_dt = tz.localize(datetime.datetime.combine(date, reopening_time), is_dst=TZ_DST)
 
-        print(closing_dt, reopening_dt)
+        cal_event = icalendar.Event()
+        cal_event.add('summary', json_item['bateau'])
+        cal_event.add('dtstart', closing_dt)
+        cal_event.add('dtend', reopening_dt)
+        cal.add_component(cal_event)
+
+    with open(ical_filename, 'wb') as fs:
+        fs.write(cal.to_ical())
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
