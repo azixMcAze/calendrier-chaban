be685009086b2e40a42554de25bceb7e4b5fdfca
mathilde <mathilde.ferrier@gmail.com>
2024-06-14 19:04:03 +0200

    changed uuid

--- calendar_data.py
+++ calendar_data.py
@@ -1,15 +1,19 @@
-from chaban import ClosureItem
 import icalendar
 
-
 CAL_MIME_TYPE = 'text/calendar'
+UID_DOMAIN = 'chaban-calendar'
 
 
-def create_calendar_item(closure_item: ClosureItem):
+def compute_uid(closure_item):
+    return f'{closure_item.closingTime.isoformat()}_{closure_item.name}@{UID_DOMAIN}'
+
+
+def create_calendar_item(closure_item):
     cal_event = icalendar.Event()
-    cal_event.add('summary', closure_item.name)
+    cal_event.add('summary', closure_item.name + ' 3')
     cal_event.add('dtstart', closure_item.closingTime)
     cal_event.add('dtend', closure_item.reopeningTime)
+    cal_event.add('uid', compute_uid(closure_item))
 
     return cal_event
 
