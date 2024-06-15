5aa5d25009786092f1475f42ba0008f4718fa61a
mathilde <mathilde.ferrier@gmail.com>
2024-06-15 14:36:31 +0200

    Removed import from

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from bridge_json_utils import API_URL
-from calendar_utils import CAL_MIME_TYPE
 from flask import Flask, Response
+import bridge_json_utils
+import calendar_utils
 import chaban_calendar
 import urllib.request
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def calendar():
-    request = urllib.request.urlopen(API_URL)
+    request = urllib.request.urlopen(bridge_json_utils.API_URL)
     json_text = request.read()
     cal_text = chaban_calendar.convert_json_to_cal(json_text)
-    return Response(cal_text, CAL_MIME_TYPE)
+    return Response(cal_text, calendar_utils.CAL_MIME_TYPE)
