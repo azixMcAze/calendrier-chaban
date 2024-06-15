f6f8633be9c3c1fb516e2edc9bce7c1236d6f31e
mathilde <mathilde.ferrier@gmail.com>
2024-06-15 14:51:14 +0200

    read data from file instead of webservice

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, Response
-import bridge_json_utils
 import calendar_utils
 import chaban_calendar
-import urllib.request
 
+
+JSON_FILENAME = 'records.json'
 
 app = Flask(__name__) 
 
 @app.route('/chaban.ics')
 def calendar():
-    request = urllib.request.urlopen(bridge_json_utils.API_URL)
-    json_text = request.read()
+    with open(JSON_FILENAME, 'r') as fs:
+        json_text = fs.read()
     cal_text = chaban_calendar.convert_json_to_cal(json_text)
     return Response(cal_text, calendar_utils.CAL_MIME_TYPE)
