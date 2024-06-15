2a0236a352d8349081bb19afc8dbfb9b6848db3d
mathilde <mathilde.ferrier@gmail.com>
2024-06-15 15:23:24 +0200

    Fixed content type

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def calendar():
     with open(JSON_FILENAME, 'r') as fs:
         json_text = fs.read()
     cal_text = chaban_calendar.convert_json_to_cal(json_text)
-    return Response(cal_text, calendar_utils.CAL_MIME_TYPE)
+    return Response(cal_text, mimetype=calendar_utils.CAL_MIME_TYPE)
