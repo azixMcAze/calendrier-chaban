0b9312ef83137e2562b0c8e0c0d09f41c3ee547e
mathilde <mathilde.ferrier@gmail.com>
2024-06-15 21:45:39 +0200

    Add week day filter

--- chaban_calendar.py
+++ chaban_calendar.py
@@ -1,11 +1,24 @@
 import json
-from bridge_json_utils import parse_bridge_json_data
+from bridge_json_utils import parse_bridge_json_data,BridgeEvent
 from calendar_utils import create_cal_from_json
+from typing import Optional, Iterable
 
 
-def convert_json_to_cal(json_text: str) -> bytes:
+def day_filter_predicate(bridge_event: BridgeEvent, day_filter: list[int]):
+    return bridge_event.closingTime.weekday() in day_filter
+
+
+def filter_by_day(bridge_data: Iterable[BridgeEvent], day_filter: Optional[list[int]]):
+    if day_filter is not None:
+        return (bridge_event for bridge_event in bridge_data if day_filter_predicate(bridge_event, day_filter))
+    else:
+        return bridge_data
+
+
+def convert_json_to_cal(json_text: str, day_filter: Optional[list[int]] = None) -> bytes:
     json_data = json.loads(json_text)
-    closure_items_list = parse_bridge_json_data(json_data)
-    cal = create_cal_from_json(closure_items_list)
+    bridge_data = parse_bridge_json_data(json_data)
+    bridge_data = filter_by_day(bridge_data, day_filter)
+    cal = create_cal_from_json(bridge_data)
 
     return cal.to_ical()
