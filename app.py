00d111484aafb56ca2dcc8fb2146f8d22e8a0e85
Mathieu Le Ber <mathieu.leber@gmail.com>
2024-09-22 12:06:39 +0200

    Moved json file and request code

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 from datetime import datetime
-import json
 from flask import Flask, Response, request
 from bridge_event import DayFilterType, filter_bridge_events
-from bridge_json_utils import parse_bridge_events
+from bridge_json_utils import load_bridge_events, download_bridge_json, save_bridge_json
 from calendar_utils import CAL_MIME_TYPE, create_cal_from_events
 
 DAYS_LETTERS = ['L', 'Ma', 'Me', 'J', 'V', 'S', 'D']
-JSON_FILENAME = 'records.json'
-API_URL = 'https://opendata.bordeaux-metropole.fr/api/explore/v2.1/catalog/datasets/previsions_pont_chaban/records?limit=100'
-
 app = Flask(__name__) 
 
 
 @app.cli.command("download")
 def download():
-    import urllib.request
-
-    request = urllib.request.urlopen(API_URL)
-    json_text = request.read().decode(request.headers.get_content_charset())
-    json_text = json.dumps(json.loads(json_text), indent=4)
-    with open(JSON_FILENAME, 'w') as fs:
-        fs.write(json_text)
+    json_data = download_bridge_json()
+    save_bridge_json(json_data)
 
 
 @app.route('/chaban.ics')
@@ -31,10 +22,7 @@ def calendar():
     day_filter = parse_day_filter(day_filter_str) if day_filter_str else None
     time_filter = parse_time_filter(time_filter_str) if time_filter_str else None
 
-    with open(JSON_FILENAME, 'r') as fs:
-        json_data = json.load(fs)
-
-    bridge_data = parse_bridge_events(json_data)
+    bridge_data = load_bridge_events()
     bridge_data = filter_bridge_events(bridge_data, day_filter, time_filter)
     cal_text = create_cal_from_events(bridge_data)
 
--- bridge_json_utils.py
+++ bridge_json_utils.py
@@ -1,6 +1,10 @@
 from datetime import date, time
+import json
 
 from bridge_event import BridgeEvent, combine_date_and_times
+
+JSON_FILENAME = 'records.json'
+API_URL = 'https://opendata.bordeaux-metropole.fr/api/explore/v2.1/catalog/datasets/previsions_pont_chaban/records?limit=100'
 
 
 def parse_bridge_json_item(json_item: dict) -> BridgeEvent:
@@ -17,3 +21,33 @@ def parse_bridge_events(json_data: dict) -> list[BridgeEvent]:
     bridge_data = [parse_bridge_json_item(json_item) for json_item in json_data['results']]
     
     return sorted(bridge_data, key=lambda item: item.start_time)
+
+
+def load_bridge_events():
+    with open(JSON_FILENAME, 'r') as fs:
+        json_data = json.load(fs)
+
+    return parse_bridge_events(json_data)
+
+
+def save_bridge_json(json_data):
+    json_text = json.dumps(json_data, indent=4)
+    with open(JSON_FILENAME, 'w') as fs:
+        fs.write(json_text)
+
+
+def download_bridge_json():
+    import urllib.request
+
+    request = urllib.request.urlopen(API_URL)
+    json_text = request.read().decode(request.headers.get_content_charset())
+
+    return json.loads(json_text)
+
+
+def download_bridge_events():
+    json_data = download_bridge_json()
+
+    return parse_bridge_events(json_data)
+
+
